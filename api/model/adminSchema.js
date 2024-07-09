801c5dedbb382765e66972768501b04c7400e77a
muh-jamiu <ganiujamiu03@gmail.com>
2024-07-09 14:38:47 +0100

    fix

--- api/model/adminSchema.js
+++ api/model/adminSchema.js
@@ -2,11 +2,11 @@ const mongoose = require("mongoose")
 
 const schema = mongoose.Schema
 
-const CodeSchema = new schema({
+const Adminchema = new schema({
     fullname : {type : String},
     phone : {type : String},
     email : {type : String, required : true},
     password : {type : String, required : true},
 },{timestamps : true})
 
-module.exports = mongoose.model("Code", CodeSchema)
+module.exports = mongoose.model("Admin", Adminchema)
