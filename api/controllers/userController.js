b832a7980898db430d6ea1652ef30628da5a8a96
muh-jamiu <ganiujamiu03@gmail.com>
2024-07-09 14:16:06 +0100

    fixes

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,5 +1,6 @@
 const userSchema = require("../model/userSchema")
 const CodeSchema = require("../model/codeSchema")
+const adminSchema = require("../model/adminSchema")
 const reviewSchema = require("../model/reviewSchema")
 const bcrypt = require("bcryptjs")
 const jwt = require("jsonwebtoken")
@@ -102,6 +103,83 @@ const sendEmailverify = (to, name, code) => {
 const generateCode = () => {
     return Array.from({ length: 4 }, () => Math.floor(Math.random() * 10)).join('');
 }
+
+const createAdmin = (req, res) => {
+    adminSchema.find({email : req.body.email})
+    .then(result => {
+       if(result.length >= 1){
+            res.status(400).json({
+                message : "user already exist"
+            })
+       }else{
+            bcrypt.hash(req.body.password, 10, (err, hash) => {
+                if(hash){
+                    const user = new adminSchema({
+                        fullname : req.body.fullname,
+                        password : hash,
+                        email : req.body.email,
+                        phone : req.body.phone,
+                    })                
+                    user.save()
+                    .then(data => {
+                        res.status(200).json({
+                            message : "Admin created successfully",
+                            user,
+                        })
+                    })
+                    .catch( err => {
+                        res.status(500).json({
+                            message: err
+                        })
+                    })
+                }else{
+                    res.status(500).json({
+                        message: "Something went wrong"
+                    })
+                }
+            })
+       }
+    })
+    .catch(err => {
+        res.status(500).json({
+            message : err
+        })
+    })
+
+   
+}
+
+const loginAdmin = (req, res) => {
+    adminSchema.find({email : req.body.email})
+    .then(user => {
+       if(user.length >= 1){
+            bcrypt.compare(req.body.password, user[0].password, (err , bol) => {
+                if(bol){
+                    res.status(200).json({
+                        message : "Admin logged  in",
+                        user: user[0],
+                    })
+                }else{
+                    res.status(403).json({
+                        message : "incorrect credentials"
+                    })
+                }
+            })
+       }
+       else{
+        res.status(404).json({
+            message : "User does not exist"
+        })
+       }
+    })
+    .catch(err => {
+        res.status(500).json({
+            error : err
+        })
+    })
+
+}
+
 
 const createUser = (req, res) => {
     userSchema.find({email : req.body.email})
