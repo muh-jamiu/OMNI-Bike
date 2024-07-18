63263676044181a9733f36d447080b928b1fc9cc
muh-jamiu <ganiujamiu03@gmail.com>
2024-07-18 12:38:11 +0100

    fixes

--- api/model/bikeSchema.js
+++ api/model/bikeSchema.js
@@ -6,6 +6,7 @@ const bikesSchema = new schema({
     bikename : {type : String, required : true},
     type : {type : String, required : true},
     BikeCode : {type : String, required : true},
+    status : {type : String, required : true},
     description : {type : String, required : true},
     image : {type : String, default : "https://capacity.rentbikesoft.pl/grafiki/oferta/256/65ddeb03ee0d8.png"},
     pricerange : {type : String, required : true},
@@ -16,6 +17,7 @@ const bikesSchema = new schema({
     wheelsize : {type : String, default : null},
     tires : {type : String, default : null},
     manufactured : {type : String, default : 2024},
+    station : {type : String}
 },{timestamps : true})
 
 module.exports = mongoose.model("Bikes", bikesSchema)
